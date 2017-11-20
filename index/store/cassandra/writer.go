c24368cd8efc543adfddbf4da8ad6e046fcfd248
Casey Haakenson <millisecond@gmail.com>
2017-11-19 19:53:00 -0500

    Clean up batch execution in Cassandra.

--- index/store/cassandra/writer.go
+++ index/store/cassandra/writer.go
@@ -30,23 +30,7 @@ func (w *Writer) ExecuteBatch(b store.KVBatch) error {
 	if !ok {
 		return fmt.Errorf("wrong type of batch")
 	}
-	//valLen := 0
-	//reader := &Reader{store: w.store}
-	//
-	//// first process merges
-	//for table, merge := range batch.merge.Merges {
-	//	batch.Set(table, kb, mergedVal)
-	//}
-	//if w.store.debug {
-	//	fmt.Println("Merges:", len(batch.merge.Merges), "Batch:", batch.batch, "Merge len: ", valLen)
-	//}
-
-	// now execute the batch
-	err := w.store.Session.ExecuteBatch(batch.batch)
-	if err != nil {
-		return err
-	}
-	return w.store.Session.ExecuteBatch(batch.counterBatch)
+	return batch.Execute()
 }
 
 func (w *Writer) Close() error {
